c5933a3230e29e58a916dcf7d4f17b6839938571
debugmebaby <debugmebaby@gmail.com>
2025-05-02 14:35:15 +0200

    selector display to work,cleanded up operate function and +-/*

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-const add = function(a, b) {
-    return a + b;
-};
+const display = document.querySelector('.display');
+let currentInput = '';
+let firstOperand = '';
+let secondOperand = '';
+let currentOperator = null;
+let resultDisplayed = false;
 
-const subtract = function (a, b){ 
-    return a - b;
-};
-
-const multiply = function (a, b){
-    return a * b;
-};
-
-const divide = function (a, b){
-    if (b === 0) {
-        return "bruh..."; 
-    }
-    return a / b;
-};
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => b === 0 ? "bruh..." : a / b;
 
 function operate(operator, a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
     switch (operator) {
-      case '+':
-        return add(a, b);
-      case '-':
-        return subtract(a, b);
-      case '*':
-        return multiply(a, b);
-      case '/':
-        return divide(a, b);
-      default:
-        return "lol NaN";
+        case '+': return add(a, b);
+        case '-': return subtract(a, b);
+        case '*': return multiply(a, b);
+        case '/': return divide(a, b);
+        default: return "lol NaN";
     }
-};
+}
